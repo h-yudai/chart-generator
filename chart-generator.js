fa9bcf1506dabfcc012dc155a275067b44c6c435
h-yudai <yudai_k89@yahoo.co.jp>
2020-11-01 19:56:13 +0900

    Fixed to class and fetch.

--- chart-generator.js
+++ chart-generator.js
@@ -1,132 +1,113 @@
-var ChartGenerator = function() {}
-
-var dataLabelPlugin = {
-	afterDatasetsDraw: function (chart, easing) {
-		// To only draw at the end of animation, check for easing === 1
-		var ctx = chart.ctx;
-
-		// Calc Percent
-		chart.data.datasets.forEach(function (dataset, i) {
-			var dataSum = 0;
-			dataset.data.forEach(function (element){
-				dataSum += Number(element);
-			});
-
-			var meta = chart.getDatasetMeta(i);
-			if (!meta.hidden) {
-				meta.data.forEach(function (element, index) {
-					// Draw the text in black, with the specified font
-					ctx.fillStyle = 'rgb(96, 96, 96)';
-
-					var fontSize = 14;
-					var fontStyle = 'normal';
-					var fontFamily = 'Helvetica Neue';
-					ctx.font = Chart.helpers.fontString(fontSize, fontStyle, fontFamily);
-
-					// Just naively convert to string for now
-					var labelString = chart.data.labels[index].toString();
-					var dataString = (Math.round(dataset.data[index] / dataSum * 100 * 10) / 10).toString() + "%";
-					//var dataString = (Math.floor(((dataset.data[index] / dataSum) * 100) + 0.5)).toString() + "%";
-
-					// Make sure alignment settings are correct
-					ctx.textAlign = 'center';
-					ctx.textBaseline = 'middle';
-
-					var padding = 0;
-					var position = element.tooltipPosition();
-					//ctx.fillText(labelString, position.x, position.y - (fontSize / 2) - padding);
-					//ctx.fillText(dataString, position.x, position.y + (fontSize / 2) - padding);
-					ctx.fillText(dataString, position.x, position.y - (fontSize / 2) - padding);
+class ChartGenerator {
+	constructor() {
+		this.chart = null
+		this.dataLabelPlugin = {
+			// データラベル表示（パイチャート）
+			afterDatasetsDraw: function(chart, easing) {
+				var ctx = chart.ctx;
+				// データセット数分繰り返し
+				chart.data.datasets.forEach(function(dataset, i) {
+					// 合計値
+					var dataSum = 0;
+					dataset.data.forEach(function(element) {
+						dataSum += Number(element);
+					});
+					var meta = chart.getDatasetMeta(i);
+					if(!meta.hidden) {
+						// データ数分繰り返し
+						meta.data.forEach(function(element, index) {
+							// 文字色
+							ctx.fillStyle = 'rgb(96, 96, 96)';
+							// フォント
+							var fontSize = 14;
+							var fontStyle = 'normal';
+							var fontFamily = 'Helvetica Neue';
+							ctx.font = Chart.helpers.fontString(fontSize, fontStyle, fontFamily);
+							// 表示文字列
+							//var labelString = chart.data.labels[index].toString();
+							//var ratioString = (Math.floor(((dataset.data[index] / dataSum) * 100) + 0.5)).toString() + "%";
+							var ratioString = (Math.round(dataset.data[index] / dataSum * 100 * 10) / 10).toString() + "%";
+							// 表示位置
+							ctx.textAlign = 'center';
+							ctx.textBaseline = 'middle';
+							var padding = 0;
+							var position = element.tooltipPosition();
+							//ctx.fillText(labelString, position.x, position.y - (fontSize / 2) - padding);
+							//ctx.fillText(ratioString, position.x, position.y + (fontSize / 2) - padding);
+							ctx.fillText(ratioString, position.x, position.y - (fontSize / 2) - padding);
+						});
+					}
 				});
 			}
-		});
+		}
 	}
-};
-
-ChartGenerator.prototype = {
-	barChart: null,
-	lineChart: null,
-	pieChart: null,
-	generateChart: function(type, domId, dataName, resources) {
-		var self = this;
+	async generateChart(type, domId, dataName, resources) {
 		var url = location.origin + "/chart/data/" + dataName + "/setting.json";
-		var xhr = new XMLHttpRequest();
-		xhr.addEventListener('load',function(){
-			var json = JSON.parse(xhr.response);
+		var prms = await fetch(url);
+		var data = await prms.json();
 
-			// Make Canvas
-			var labelAry = json.labels.split(',');
+		// Make Chart
+		var labelAry = data.labels.split(',');
+		var ctx = document.getElementById(domId).getContext('2d');
+		switch (type) {
+			case 'bar':
+				this.makeBarChart(ctx, data.title, labelAry, data.ylabel);
+				break;
+			case 'stacked-bar':
+				this.makeStackedBarChart(ctx, data.title, labelAry, data.ylabel);
+				break;
+			case 'line':
+				this.makeLineChart(ctx, data.title, labelAry, data.ylabel);
+				break;
+			case 'pie':
+				this.makePieChart(ctx, data.title, labelAry);
+				break;
+			default:
+				console.log('No Type');
+				return;
+		}
+
+		// Add Data
+		var resourceAry = resources.split(',');
+		for (let i = 0; i < resourceAry.length; ++i) {
+			var resource = resourceAry[i];
+			url = location.origin + "/chart/data/" + dataName + "/" + resource + ".json";
+			prms = await fetch(url);
+			data = await prms.json();
 			switch (type) {
 				case 'bar':
-					self.makeBarCanvas(domId, json.title, labelAry, json.ylabel);
+					this.addBarData(data);
+					break;
+				case 'stacked-bar':
+					this.addStackedBarData(data);
 					break;
 				case 'line':
-					self.makeLineCanvas(domId, json.title, labelAry, json.ylabel);
+					this.addLineData(data);
 					break;
 				case 'pie':
-					self.makePieCanvas(domId, json.title, labelAry);
+					this.addPieData(data);
 					break;
-				default:
-					console.log('No Type');
 			}
-
-			// Add Data
-			var resourceAry = resources.split(',');
-			for (let i = 0; i < resourceAry.length; ++i) {
-				var resource = resourceAry[i];
-				switch (type) {
-					case 'bar':
-						self.addBarData(dataName, resource);
-						break;
-					case 'line':
-						self.addLineData(dataName, resource);
-						break;
-					case 'pie':
-						self.addPieData(dataName, resource);
-						break;
-					default:
-						console.log('No Type');
-				}
-			}
-
-			// Update Chart
-			if(self.barChart) {
-				self.barChart.update();				
-			}
-			if(self.lineChart) {
-				self.lineChart.update();				
-			}
-			if(self.pieChart) {
-				self.pieChart.update();				
-			}
-		},false);
-		xhr.open("GET", url, true);
-		xhr.send(null);
-	},
-	makeBarCanvas: function(domId, title, labelAry, ylabel) {
-		if(this.barChart) {
-			this.barChart.destroy();
 		}
-		var ctx = document.getElementById(domId).getContext('2d');
-		this.barChart = new Chart(ctx, {
+
+		// Update Chart
+		this.chart.update();
+	}
+	makeBarChart(ctx, title, labelAry, ylabel) {
+		this.chart = new Chart(ctx, {
 			type: 'bar',
 			data: {
 				labels: labelAry,
 				datasets: []
 			},
 			options: {
+				animation: {duration: 0},
 				responsive: true,
-				legend: {
-					position: 'top',
-				},
+				legend: {position: 'top'},
+				title: {display: false, text: title},
 				scales: {
-					xAxes: [{
-						display: true
-					}],
 					yAxes: [{
-						display: true,
 						scaleLabel: {
-							display: true,
 							labelString: ylabel
 						},
 						ticks: {
@@ -137,9 +118,44 @@ ChartGenerator.prototype = {
 						}
 					}]
 				},
-				title: {
-					display: false,
-					text: title
+				tooltips: {
+					callbacks: {
+							label: function(tooltipItem, data){
+							return tooltipItem.yLabel.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+						}
+					}
+				}
+			}
+		});
+	}
+	makeStackedBarChart(ctx, title, labelAry, ylabel) {
+		this.chart = new Chart(ctx, {
+			type: 'bar',
+			data: {
+				labels: labelAry,
+				datasets: []
+			},
+			options: {
+				animation: {duration: 0},
+				responsive: true,
+				legend: {position: 'top'},
+				title: {display: false, text: title},
+				scales: {
+					xAxes: [{
+						stacked: true
+					}],
+					yAxes: [{
+						stacked: true,
+						scaleLabel: {
+							labelString: ylabel
+						},
+						ticks: {
+							maxTicksLimit: 6,
+							callback: function(label, index, labels) {
+								return label.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+							}
+						}
+					}]
 				},
 				tooltips: {
 					callbacks: {
@@ -150,31 +166,22 @@ ChartGenerator.prototype = {
 				}
 			}
 		});
-	},
-	makeLineCanvas: function(domId, title, labelAry, ylabel) {
-		if(this.lineChart) {
-			this.lineChart.destroy();
-		}
-		var ctx = document.getElementById(domId).getContext('2d');
-		this.lineChart = new Chart(ctx, {
+	}
+	makeLineChart(ctx, title, labelAry, ylabel) {
+		this.chart = new Chart(ctx, {
 			type: 'line',
 			data: {
 				labels: labelAry,
 				datasets: []
 			},
 			options: {
+				animation: {duration: 0},
 				responsive: true,
-				legend: {
-					position: 'top',
-				},
+				legend: {position: 'top'},
+				title: {display: false, text: title},
 				scales: {
-					xAxes: [{
-						display: true
-					}],
 					yAxes: [{
-						display: true,
 						scaleLabel: {
-							display: true,
 							labelString: ylabel
 						},
 						ticks: {
@@ -185,10 +192,6 @@ ChartGenerator.prototype = {
 						}
 					}]
 				},
-				title: {
-					display: false,
-					text: title
-				},
 				tooltips: {
 					callbacks: {
 							label: function(tooltipItem, data){
@@ -198,137 +201,96 @@ ChartGenerator.prototype = {
 				}
 			}
 		});
-	},
-	makePieCanvas: function(domId, title, labelAry) {
-		if(this.pieChart) {
-			this.pieChart.destroy();
-		}
-		var ctx = document.getElementById(domId).getContext('2d');
-		this.pieChart = new Chart(ctx, {
+	}
+	makePieChart(ctx, title, labelAry) {
+		this.chart = new Chart(ctx, {
 			type: 'pie',
 			data: {
 				labels: labelAry,
 				datasets: []
 			},
 			options: {
+				animation: {duration: 0},
 				responsive: true,
+				legend: {position: 'top'},
+				title: {display: false, text: title},
 				maintainAspectRatio: false,
-				title: {
-					display: false,
-					text: title
-				},
 				tooltips: {
 					callbacks: {
 						label: function(tooltipItem, data) {
-							//get the concerned dataset
 							var dataset = data.datasets[tooltipItem.datasetIndex];
-							//calculate the total of this data set
 							var total = dataset.data.reduce(function(previousValue, currentValue, currentIndex, array) {
 								return Number(previousValue) + Number(currentValue);
 							});
-							//get the current items value
 							var label = data.labels[tooltipItem.index];
 							var currentValue = Number(dataset.data[tooltipItem.index]);
-							//calculate the percentage based on the total and current item, also this does a rough rounding to give a whole number
 							//var percentage = Math.floor(((currentValue/total) * 100) + 0.5);
 							var percentage = Math.round(currentValue / total * 100 * 10) / 10;
-							return label + " : " + percentage + '%';
+							return label + ' : ' + percentage + '%';
 
 							/*
 							var label = data.labels[tooltipItem.index];
 							var dataset = data.datasets[tooltipItem.datasetIndex];
-							var valueStr = dataset.data[tooltipItem.index].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');	
+							var valueStr = dataset.data[tooltipItem.index].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 							return label + " : " + valueStr;
 							*/
 						}
 					}
 				}
 			},
-			//plugins: [dataLabelPlugin]
+			//plugins: [this.dataLabelPlugin]
 		});
-	},
-	addBarData: function(dataName, resource) {
-		var self = this;
-		var url = location.origin + "/chart/data/" + dataName + "/" + resource + ".json";
-		var xhr = new XMLHttpRequest();
-		xhr.addEventListener('load',function() {
-			var json = JSON.parse(xhr.response);
-			var dataset = {
-				label: json.label,
-				backgroundColor: Chart.helpers.color(json.color).alpha(0.5).rgbString(),
-				borderColor: Chart.helpers.color(json.color).alpha(0.5).rgbString(),
-				borderWidth: 1,
-				data: json.values.split(',')
-			}
-			self.barChart.data.datasets.push(dataset);
-		},false);
-		xhr.open("GET", url, false);
-		xhr.send(null);
-/* 		var url = location.origin + "/chart/data/" + dataName + "/" + resource + ".json";
-		fetch(url)
-			.then((response) => {
-				if(response.ok) {
-					return response.json();
-				} else {
-					throw new Error();
-				}
-			})
-			.then((data) => {
-				var dataset = {
-					label: data.label,
-					backgroundColor: Chart.helpers.color(data.color).alpha(0.5).rgbString(),
-					borderColor: Chart.helpers.color(data.color).alpha(0.5).rgbString(),
-					borderWidth: 1,
-					data: data.values.split(',')
-				}
-				self.chart.data.datasets.push(dataset);
-			})
-			.catch((error) => console.log(error)); */
-	},
-	addLineData: function(dataName, resource) {
-		var self = this;
-		var url = location.origin + "/chart/data/" + dataName + "/" + resource + ".json";
-		var xhr = new XMLHttpRequest();
-		xhr.addEventListener('load',function() {
-			var json = JSON.parse(xhr.response);
-			var dataset = {
-				label: json.label,
-				fill: false,
-				lineTension: 0,
-				pointRadius: 5,
-				pointHoverRadius: 8,
-				pointHoverBackgroundColor: Chart.helpers.color(json.color).alpha(0.5).rgbString(),
-				backgroundColor: Chart.helpers.color(json.color).alpha(0.5).rgbString(),
-				borderColor: Chart.helpers.color(json.color).alpha(0.5).rgbString(),
-				borderWidth: 1,
-				data: json.values.split(',')
-			}
-			self.lineChart.data.datasets.push(dataset);
-		},false);
-		xhr.open("GET", url, false);
-		xhr.send(null);
-	},
-	addPieData: function(dataName, resource) {
-		var self = this;
-		var url = location.origin + "/chart/data/" + dataName + "/" + resource + ".json";
-		var xhr = new XMLHttpRequest();
-		xhr.addEventListener('load',function() {
-			var json = JSON.parse(xhr.response);
-			var dataAry = json.values.split(',')
-			var colorAry = json.color.split(';')
-			var colorAlphaAry = [];
-			for (let i = 0; i < colorAry.length; ++i) {
-				colorAlphaAry.push(Chart.helpers.color(colorAry[i]).alpha(0.5).rgbString())
-			}
-			var dataset = {
-				label: json.label,
-				backgroundColor: colorAlphaAry,
-				hoverBackgroundColor: colorAlphaAry,
-				data: dataAry
-			}
-			self.pieChart.data.datasets.push(dataset);
-		},false);
-		xhr.open("GET", url, false);
-		xhr.send(null);
+	}
+	addBarData(data) {
+		var color = Chart.helpers.color(data.color).alpha(0.5).rgbString();
+		var dataset = {
+			label: data.label,
+			backgroundColor: color,
+			borderColor: color,
+			borderWidth: 1,
+			data: data.values.split(',')
+		}
+		this.chart.data.datasets.push(dataset);
+	}
+	addStackedBarData(data) {
+		var color = Chart.helpers.color(data.color).alpha(0.5).rgbString();
+		var dataset = {
+			label: data.label,
+			backgroundColor: color,
+			borderColor: color,
+			borderWidth: 1,
+			data: data.values.split(',')
+		}
+		this.chart.data.datasets.push(dataset);
+	}
+	addLineData(data) {
+		var color = Chart.helpers.color(data.color).alpha(0.5).rgbString();
+		var dataset = {
+			label: data.label,
+			fill: false,
+			lineTension: 0,
+			pointRadius: 5,
+			pointHoverRadius: 8,
+			pointHoverBackgroundColor: color,
+			backgroundColor: color,
+			borderColor: color,
+			borderWidth: 1,
+			data: data.values.split(',')
+		}
+		this.chart.data.datasets.push(dataset);
+	}
+	addPieData(data) {
+		var colorAry = data.color.split(';');
+		var colorAlphaAry = [];
+		for (let i = 0; i < colorAry.length; ++i) {
+			colorAlphaAry.push(Chart.helpers.color(colorAry[i]).alpha(0.5).rgbString())
+		}
+		var dataset = {
+			label: data.label,
+			backgroundColor: colorAlphaAry,
+			hoverBackgroundColor: colorAlphaAry,
+			data: data.values.split(',')
+		}
+		this.chart.data.datasets.push(dataset);
 	}
 }
